55961fa4f36e380a8e2b6f6379fe8ee95930fb5a
k-murakami0609 <dadgac@gmail.com>
2019-05-07 16:43:40 +0900

    ローディングを実装した

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -2,18 +2,34 @@ import * as React from "react";
 import { useState, useEffect } from "react";
 
 import { Header, SearchResult } from "../organisms";
+import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import ApiClient, { SearchResponse } from "../../services/api/apiClient";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const useStyles = makeStyles(theme => ({
+  progress: {
+    margin: theme.spacing(2)
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   const [query, setQuery] = useState("花火");
   const [searchResult, setSearchResult] = useState<SearchResponse[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await ApiClient.search(query);
-      setSearchResult(result);
+      setIsLoading(true);
+      try {
+        const result = await ApiClient.search(query);
+        setSearchResult(result);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     fetchData();
@@ -24,7 +40,11 @@ function App() {
       <CssBaseline />
       <Header onSubmit={setQuery} />
       <Container>
-        <SearchResult items={searchResult} />
+        {isLoading ? (
+          <CircularProgress className={classes.progress} />
+        ) : (
+          <SearchResult items={searchResult} />
+        )}
       </Container>
     </React.Fragment>
   );
